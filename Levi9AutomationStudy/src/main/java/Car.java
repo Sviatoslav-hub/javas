9c2e6253e7bec9aff4b2f09a39061b8aff6417ab
Sviatoslav Yurkiv <s.yurkiv@levi9.com>
2021-11-11 18:23:24 +0200

    add new class

--- Levi9AutomationStudy/src/main/java/Car.java
+++ Levi9AutomationStudy/src/main/java/Car.java
@@ -10,14 +10,25 @@ public class Car {
         this.color = color;
         this.type = type;
     }
+    public void methodStart() {
+        System.out.println("Car Start!");
+    }
+    public void methodStop(){
+        System.out.println("Car Stops!");
+    }
+    public void methodTurnLeft(){
+        System.out.println("Car turned Left");
+    }
+    public void methodTurnRight(){
+        System.out.println("Car turned Right");
+    }
 
     public static void main(String[] args) {
         Car Volvo = new Car(500, 4, "D class", "Black");
+        Volvo.methodStart();
+        Volvo.methodTurnLeft();
+        Volvo.methodTurnRight();
+        Volvo.methodStop();
     }
-
-    public static void methodStart() {
-        System.out.println("Car drive!");
-    }
-
 }
 
