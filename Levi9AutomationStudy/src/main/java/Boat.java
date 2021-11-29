0d8258b5cb261c83be1c70cd2416f9ed3b19303b
Sviatoslav Yurkiv <s.yurkiv@levi9.com>
2021-11-29 18:35:03 +0200

    boatcounter

--- Levi9AutomationStudy/src/main/java/Boat.java
+++ Levi9AutomationStudy/src/main/java/Boat.java
@@ -1,6 +1,21 @@
 public class Boat extends PublicTransport {
 
     private int length;
+    private String name;
+    private int horsPower;
+    private static int boatCount;
+
+
+    public Boat(String destination, int places, int transferCost, int length, String name, int horsPower) {
+        this.length = length;
+        this.name = name;
+        this.horsPower = horsPower;
+        this.destination = destination;
+        this.places = places;
+        this.transferCost = transferCost;
+        boatCount++;
+    }
+
 
     public String getName() {
         return name;
@@ -9,21 +24,6 @@ public class Boat extends PublicTransport {
     public void setName(String name) {
         this.name = name;
     }
-
-    private  String name;
-    private int horsPower;
-
-    public Boat(String destination, int places, int transferCost, int length, String name, int horsPower){
-        this.length = length;
-        this.name = name;
-        this.horsPower = horsPower;
-        this.destination = destination;
-        this.places = places;
-        this.transferCost = transferCost;
-    }
-
-
-
 
     @Override
     protected void methodDelivery() {
@@ -39,23 +39,29 @@ public class Boat extends PublicTransport {
 
     @Override
     public void methodTransferCost() {
-        System.out.println("This transfer costs " + transferCost +" dollars");
+        System.out.println("This transfer costs " + transferCost + " dollars");
 
     }
 
-    public void methodLength(){
-        System.out.println("My length is "+ length + " meters");
+    public static int getBoatCount() {
+        return boatCount;
     }
+
+    public void methodLength() {
+        System.out.println("My length is " + length + " meters");
+    }
+
     public void methodName() {
         System.out.println("My Name is " + name);
     }
 
-        public void methodHorsePower(){
+    public void methodHorsePower() {
         System.out.println("I have " + horsPower + " Powers");
     }
 
     public static void main(String[] args) {
         Boat Kater = new Boat("Valhalla", 50, 2, 3, "Drakar", 500);
+        Boat Kater2 = new Boat("Lviv", 40, 40,40,"Choven",400);
         Kater.methodDelivery();
         Kater.methodHorsePower();
         Kater.methodLength();
@@ -65,6 +71,7 @@ public class Boat extends PublicTransport {
         System.out.println(Kater.getName());
         Kater.setName("Killer");
         System.out.println(Kater.getName());
+        System.out.println(getBoatCount());
     }
 
 
